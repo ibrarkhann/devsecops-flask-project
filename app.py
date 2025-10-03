bcb000c4980d2f1844e4679a90efc1b7ccca05c2
Ubuntu <ubuntu@ip-172-31-16-134.ec2.internal>
2025-10-03 15:12:24 +0000

    latest changes

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-app.config['SECRET_KEY'] = 'badsecret123'
 #!/usr/bin/env python3
 
+from flask import Flask, render_template, render_template_string, request, redirect
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_login import LoginManager
-from flask import Flask, render_template, render_template_string, request, redirect
 from db_seed import setup_db
 from routes import init
 
 app = Flask(__name__)
-app.secret_key = "super secret key"
+
+# Security: Use config properly
+app.config['SECRET_KEY'] = 'supersecretkey'
 app.config["BOOTSTRAP_SERVE_LOCAL"] = True
 app.config["CKEDITOR_SERVE_LOCAL"] = True
+
+# Initialize extensions
 bootstrap = Bootstrap5(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-ckeditor = CKEditor()
+ckeditor = CKEditor(app)
 
-ckeditor.init_app(app)
+# Initialize app-specific modules
 init()
 setup_db()
 
@@ -33,3 +36,4 @@ def page_not_found(error):
         f"{error}. Requested URL was {request.path}"
     )
     return render_template("404.html", detailed_message=detailed_message)
+
